cf084749fe0c692289f8a76838a31ee73667d257
kevin07696 <kevinlam07696@gmail.com>
2025-01-03 01:05:23 +0000

    Removing custom middleware chain for handler

--- handlers/receipt/handler.go
+++ handlers/receipt/handler.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/kevin07696/receipt-processor/domain/receipt"
-	"github.com/kevin07696/receipt-processor/handlers"
 )
 
 type Handler struct {
@@ -12,16 +11,13 @@ type Handler struct {
 	receiptAPI receipt.IReceiptProcessorService
 }
 
-func Handle(router *http.ServeMux, receiptAPI receipt.IReceiptProcessorService, middlewares ...handlers.Middleware) *Handler {
+func Handle(router *http.ServeMux, receiptAPI receipt.IReceiptProcessorService) *Handler {
 	h := &Handler{
 		router:     router,
 		receiptAPI: receiptAPI,
 	}
 
 	h.initAppRoutes()
-
-	middlewareChain := handlers.MiddlewareChain(middlewares...)
-	middlewareChain(h.router)
 
 	return h
 }
